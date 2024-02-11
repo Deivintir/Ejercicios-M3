f6d68920947c4c627ccf2f2392bfbb16d78114d6
Deivintir <d4vidyeva@gmail.com>
2024-02-11 15:23:20 +0100

    Ejercicio 1 Modulo 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /**• En el archivo main.ts, declara una variable con tipo “string”, otra con
 tipo “number” y otra con tipo “boolean”. */
-let user:string;
-let age:number;
-let pass:boolean;
+let user;
+let age;
+let pass;
 /**• Inicializa las variables con valores del tipo correspondiente. */
 user = 'David';
 age = 39;
@@ -14,8 +14,8 @@ console.log(pass);
 /**• Transpila el archivo main.ts a un archivo main.js. (tsc ts/main.ts --outFile js/main.js --target ES2015 --watch).
  * • Comprueba su correcta ejecución en el navegador.
  * • Demuestra que, al cambiar el valor de una variable a un tipo distinto, se produce un error. */
-let doughter:string;
+let doughter;
 //doughter = 39; //<-Descomentar esta linea para ver el error de transpilación
 /**• Implementa el tipo “any” para corregir el error */
-let son:any;
+let son;
 son = 0;
